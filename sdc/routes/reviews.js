5b8061a4ca3632c39ed19234d4a1d2d443a698bb
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2022-02-12 11:05:30 -0800

    Created server on 3051 for reviews

---
+++ sdc/routes/reviews.js
@@ -0,0 +1,85 @@
+'use strict'
+
+var express = require('express');
+
+var apiv1 = express.Router();
+
+apiv1.get('/', function(req, res) {
+  res.send('Hello from APIv1 root route.');
+});
+
+apiv1.get('/reviews', function(req, res) {
+  let answer = {
+    "product": "2",
+    "page": 0,
+    "count": 5,
+    "results": [
+      {
+        "review_id": 5,
+        "rating": 3,
+        "summary": "I'm enjoying wearing these shades",
+        "recommend": false,
+        "response": null,
+        "body": "Comfortable and practical.",
+        "date": "2019-04-14T00:00:00.000Z",
+        "reviewer_name": "shortandsweeet",
+        "helpfulness": 5,
+        "photos": [{
+            "id": 1,
+            "url": "urlplaceholder/review_5_photo_number_1.jpg"
+          },
+          {
+            "id": 2,
+            "url": "urlplaceholder/review_5_photo_number_2.jpg"
+          },
+
+        ]
+      },
+      {
+        "review_id": 3,
+        "rating": 4,
+        "summary": "I am liking these glasses",
+        "recommend": false,
+        "response": "Glad you're enjoying the product!",
+        "body": "They are very dark. But that's good because I'm in very sunny spots",
+        "date": "2019-06-23T00:00:00.000Z",
+        "reviewer_name": "bigbrotherbenjamin",
+        "helpfulness": 5,
+        "photos": [],
+      },
+
+    ]
+  }
+  res.send(answer);
+});
+
+apiv1.get('/meta', function(req, res) {
+  let answer = {
+    "product_id": "2",
+    "ratings": {
+      2: 1,
+      3: 1,
+      4: 2
+    },
+    "recommended": {
+      0: 5
+    },
+    "characteristics": {
+      "Size": {
+        "id": 14,
+        "value": "4.0000"
+      },
+      "Width": {
+        "id": 15,
+        "value": "3.5000"
+      },
+      "Comfort": {
+        "id": 16,
+        "value": "4.0000"
+      }
+  }
+}
+  res.send(answer);
+});
+
+module.exports = apiv1;
