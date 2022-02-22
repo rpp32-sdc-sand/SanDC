1142701ebf329df392d2d29a978302de94c9f8f1
Nate Rana <n8.rana@gmail.com>
2022-02-22 16:08:25 -0600

    fixed lost etl stuff

--- sdc/sdc-overview/sdc-dbs/pgschema.sql
+++ sdc/sdc-overview/sdc-dbs/pgschema.sql
@@ -18,40 +18,33 @@ CREATE TABLE sdc.products.product (
   UNIQUE (product_id)
 );
 
+
 CREATE TABLE sdc.products.styles (
   id SERIAL,
   styles_id INT,
   product_id INT,
+  name VARCHAR (50),
+  sale_price VARCHAR (11),
+  original_price VARCHAR (11),
+  style_default BOOLEAN,
+  photos TEXT,
+  skus_id INT,
   PRIMARY KEY (id),
   UNIQUE (styles_id),
   CONSTRAINT fk_products
     FOREIGN KEY (product_id)
-      REFERENCES sdc.products.product(product_id)
-);
-
-CREATE TABLE sdc.products.style (
-  id SERIAL,
-  style_id INT,
-  styles_fk INT,
-  skus_id INT,
-  name VARCHAR (50),
-  original_price VARCHAR (11),
-  sale_price VARCHAR (11),
-  style_default BOOLEAN,
-  photos TEXT,
-
-  PRIMARY KEY (id),
-  UNIQUE (style_id),
-  CONSTRAINT fk_styles
-    FOREIGN KEY (styles_fk)
-      REFERENCES sdc.products.styles(styles_id)
+    REFERENCES sdc.products.product(product_id)
 );
 
 CREATE TABLE sdc.products.skus (
   id SERIAL,
   skus_id INT,
+  styles_id INT,
+  size TEXT,
   quantity INT,
-  size INT,
   PRIMARY KEY (id),
-  UNIQUE (skus_id)
+  UNIQUE (skus_id),
+  CONSTRAINT fk_styles
+    FOREIGN KEY (styles_id)
+    REFERENCES sdc.products.styles(styles_id)
 );
