912bb6dde77b52c5353c16fdbf00abe4c3f28e06
Nate Rana <n8.rana@gmail.com>
2022-03-14 02:49:31 -0500

    changed addresses to point to right addresses

--- Project-Atelier-main/server/ProductApi.js
+++ Project-Atelier-main/server/ProductApi.js
@@ -3,48 +3,29 @@ const gitToken = require('../config.js');
 
 
 const getSpecificProduct = (productId) => {
-  productId = 1000012;
   let options = {
     method: 'GET',
-    // hr api
-    // url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${productId}`,
-    // local test
-    url: `http://localhost:3050/fec2/hr-rpp/products/${productId}`,
-    // sdc api
-    // url: `http://3.82.163.215:3050/fec2/hr-rpp/products/${productId}`,
+    url: `http://3.82.163.215:3050/fec2/hr-rpp/products/${productId}`,
     headers: { Authorization: gitToken.Token },
   };
   return axios(options)
     .then((response) => {
-      // console.log(response.data);
       return response.data;
     })
     .catch((error) => {
-      // console.log('options 18:', options);
       console.log('error:', error);
       return error;
     });
 };
 
 const getProductStyles = (productId) => {
-  productId = 1;
   let options = {
     method: 'GET',
-    // url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${productId}/styles`,
-    url: `http://localhost:3050/fec2/hr-rpp/products/${productId}/styles`,
-    // url: `http://3.82.163.215:3050/fec2/hr-rpp/products/${productId}/styles`,
+    url: `http://3.82.163.215:3050/fec2/hr-rpp/products/${productId}/styles`,
     headers: { Authorization: gitToken.Token },
   };
   return axios(options)
     .then((response) => {
-      // console.log('client styles 31: ');
-      // console.log(response.data);
-      // console.log('>>>>>>>>>>>>>>>>>>>');
-      // console.log('response.data', response.data.results);
-      // console.log('skus0:', response.data.results[0].skus);
-      // // console.log('skus1:', response.data.results[1].skus);
-      // // console.log('skus0 data:', response.data.results[0].skus['2313078']);
-      // console.log('<<<<<<<<<<<<<<<<<<');
       return response.data;
     });
 };
--- sdc/routes/db.js
+++ sdc/routes/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
+  user: 'ubuntu',
   password: '',
   database: 'sdc',
-  // host: '52.91.62.180',
-  host: '127.0.0.1',
+  host: '52.91.62.180',
+  // host: '127.0.0.1',
   port: '5432'
 });
 
